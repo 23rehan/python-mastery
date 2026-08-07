7fdb51d7aca3b7edbfa638ed8ca5e99c9c9cf367
Rehan Khan <167982264+23rehan@users.noreply.github.com>
2026-08-07 17:37:01 +0500

    weekly revision
    Added a program for revision covering input, arithmetic operations, if-else, and logical operators.

--- 01_Basics/challenge.py
+++ 01_Basics/challenge.py
@@ -1,1 +1,42 @@
+# Today i make program of my past topics for revision purpose 
+
+# Topics: input(), type conversion, arithmetic operators.
+
+first_num = int(input("Enter the any number:"))
+
+second_num = int(input("Enter the any number again:"))
+
+print("The sum of these numbers is:",first_num + second_num)
+
+print("The difference of these number is:",first_num - second_num)
+
+print("The Maltiple of these numbers is:",first_num * second_num)
+
+print("The division of these numbers is:",first_num / second_num) 
+
+-----
+
+# Topics: if-else
+
+user_age = int(input("Enter your age:"))
+if user_age>=18:
+    print("you can vote!")
+else:
+    print("you can't vote!")
+
+------
+
+# Topics: if, elif, logical operators.
+
+
+user_marks = int(input("Enter your marks:"))
+if user_marks < 0 or user_marks > 100:
+    print("invalid marks!try again")
+elif user_marks >= 40:
+    print("you'r pass!")
+else:
+    print(" you'r fail!")
+
+
+
 
